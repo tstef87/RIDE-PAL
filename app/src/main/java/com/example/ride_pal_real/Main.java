68559c4cd33af738736044d36f42c879c2d95e33
tstef87 <65368155+tstef87@users.noreply.github.com>
2021-11-11 15:13:27 -0500

    logout

--- app/src/main/java/com/example/ride_pal_real/Main.java
+++ app/src/main/java/com/example/ride_pal_real/Main.java
@@ -2,13 +2,30 @@ package com.example.ride_pal_real;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Main extends AppCompatActivity {
+
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        logout = (Button) findViewById(R.id.signOut);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(Main.this, SignInPage.class));
+            }
+        });
     }
 }
