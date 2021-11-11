cb06cfaaeec84d737ed843ffce00ca53b06f519c
tstef87 <65368155+tstef87@users.noreply.github.com>
2021-11-11 14:04:50 -0500

    email verification

--- app/src/main/java/com/example/ride_pal_real/SignInPage.java
+++ app/src/main/java/com/example/ride_pal_real/SignInPage.java
@@ -16,6 +16,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignInPage extends AppCompatActivity implements View.OnClickListener {
 
@@ -89,7 +90,15 @@ public class SignInPage extends AppCompatActivity implements View.OnClickListene
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
-                    startActivity(new Intent(SignInPage.this, Main.class));
+
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    if(user.isEmailVerified()) {
+                        startActivity(new Intent(SignInPage.this, Main.class));
+                    }else{
+                        user.sendEmailVerification();
+                        Toast.makeText(SignInPage.this, "check email for varification", Toast.LENGTH_LONG).show();
+                    }
+
                     progressBar.setVisibility(View.INVISIBLE);
                 }
                 else{
