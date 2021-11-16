b78a75c8e5c9fa8edb0968ce792e8274805edb02
tstef87 <65368155+tstef87@users.noreply.github.com>
2021-11-16 18:47:46 -0500

    homepage layout

--- app/src/main/java/com/example/ride_pal_real/AccountInfoActivity.java
+++ app/src/main/java/com/example/ride_pal_real/AccountInfoActivity.java
@@ -2,25 +2,38 @@ package com.example.ride_pal_real;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ride_pal_real.databinding.ActivityAccountInfoBinding;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AccountInfoActivity extends AppCompatActivity  {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityAccountInfoBinding binding;
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userId;
+
+
 
 
     @Override
@@ -49,6 +62,13 @@ public class AccountInfoActivity extends AppCompatActivity  {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_account_info);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+
+
+
+
+
+
     }
 
     @Override
--- app/src/main/java/com/example/ride_pal_real/ui/gallery/GalleryViewModel.java
+++ app/src/main/java/com/example/ride_pal_real/ui/gallery/GalleryViewModel.java
@@ -4,13 +4,37 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class GalleryViewModel extends ViewModel {
+
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userId;
+
 
     private MutableLiveData<String> mText;
 
     public GalleryViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+
+
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userId = user.getUid();
+
+        mText.setValue(user.getEmail());
+
+
+
+
+
+
     }
 
     public LiveData<String> getText() {
