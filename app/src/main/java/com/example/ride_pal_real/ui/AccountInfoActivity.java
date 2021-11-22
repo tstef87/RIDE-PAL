43ff284990a66fe64c2a218f66e682d1f41571b7
tstef87 <ts719104@sju.edu>
2021-11-22 11:24:31 -0500

    name added to nav bar

--- app/src/main/java/com/example/ride_pal_real/ui/AccountInfoActivity.java
+++ app/src/main/java/com/example/ride_pal_real/ui/AccountInfoActivity.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
@@ -15,12 +17,28 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.ride_pal_real.R;
 import com.example.ride_pal_real.databinding.ActivityAccountInfoBinding;
+import com.example.ride_pal_real.sign_in.User;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Locale;
 
 public class AccountInfoActivity extends AppCompatActivity  {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityAccountInfoBinding binding;
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+
+    private String userId;
+
 
 
 
@@ -43,21 +61,50 @@ public class AccountInfoActivity extends AppCompatActivity  {
         DrawerLayout drawer = binding.drawerLayout;
 
 
-
-
-
-
-
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_map, R.id.nav_logout, R.id.nav_profile)
+                R.id.nav_map, R.id.nav_logout, R.id.nav_profile, R.id.nav_rides)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_account_info);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        TextView navUsername = (TextView) headerView.findViewById(R.id.navFullName);
+
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userId = user.getUid();
+
+        reference.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+
+                if (userProfile != null) {
+                    String fn = userProfile.firstname.toUpperCase(Locale.ROOT);
+                    String ln = userProfile.lastname.toUpperCase(Locale.ROOT);
+                    String full = fn + " " + ln;
+                    String ema = userProfile.email;
+
+                    navUsername.setText(full);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
 
 
     }
@@ -66,6 +113,7 @@ public class AccountInfoActivity extends AppCompatActivity  {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.account_info, menu);
+
         return true;
     }
 
