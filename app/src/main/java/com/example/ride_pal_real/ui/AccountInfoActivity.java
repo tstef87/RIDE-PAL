1d4913fc52e5647c2c176ef42e62c01b7479033b
tstef87 <ts719104@sju.edu>
2021-11-22 09:12:04 -0500

    frends list

--- app/src/main/java/com/example/ride_pal_real/ui/AccountInfoActivity.java
+++ app/src/main/java/com/example/ride_pal_real/ui/AccountInfoActivity.java
@@ -24,11 +24,6 @@ public class AccountInfoActivity extends AppCompatActivity  {
 
 
 
-
-
-
-
-
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,7 +52,7 @@ public class AccountInfoActivity extends AppCompatActivity  {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_map, R.id.nav_rides, R.id.nav_logout, R.id.nav_profile)
+                R.id.nav_map, R.id.nav_logout, R.id.nav_profile, R.id.nav_rides)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_account_info);
