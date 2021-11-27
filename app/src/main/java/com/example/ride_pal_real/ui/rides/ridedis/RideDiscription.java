43329188c7c3dd306a850a695250bc42ae25519a
tstef87 <65368155+tstef87@users.noreply.github.com>
2021-11-27 15:39:34 -0500

    ridedis button functionality

--- app/src/main/java/com/example/ride_pal_real/ui/rides/ridedis/RideDiscription.java
+++ app/src/main/java/com/example/ride_pal_real/ui/rides/ridedis/RideDiscription.java
@@ -2,8 +2,12 @@ package com.example.ride_pal_real.ui.rides.ridedis;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.ride_pal_real.R;
+import com.example.ride_pal_real.ui.AccountInfoActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RideDiscription extends AppCompatActivity {
 
@@ -11,6 +15,22 @@ public class RideDiscription extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ride_discription);
+
+        Button back = (Button) findViewById(R.id.ride_dis_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RideDiscription.this, AccountInfoActivity.class));
+            }
+        });
+
+        Button accept = (Button) findViewById(R.id.accept);
+        accept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
 
 
